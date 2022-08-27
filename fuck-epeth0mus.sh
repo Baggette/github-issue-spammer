d5c6cbcf267f49049b57d98fb53bb144eabd861f
Baggette <play.email200620062006@gmail.com>
2022-08-27 10:46:39 -0400

    stuff

--- fuck-epeth0mus.sh
+++ fuck-epeth0mus.sh
@@ -3,8 +3,7 @@ sudo apt update && sudo apt upgrade -y && sudo apt install gh
     
 echo "Have you configured gh yet? y/n"
 read y
-if [[$y == "n"]]
-    then 
+if [[$y == "n"]]; then
         echo "It is reccomened to have a personal access token ready"
         sleep 2
         gh auth login
