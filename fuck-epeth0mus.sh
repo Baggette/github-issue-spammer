58f45be20d6eb0a482f992759b9107c9f0b0d7fc
Baggette <play.email200620062006@gmail.com>
2022-08-27 11:10:06 -0400

    Fuck epeth0mus Indeed

--- fuck-epeth0mus.sh
+++ fuck-epeth0mus.sh
@@ -15,7 +15,20 @@ if [[ $y == "n" ]]; then
 elif [[ $y == "y" ]]; then
         while true
         do
-            gh issue create --repo https://github.com/Baggette/fuck-epeth0mus --title "test2" -b ""
-            sleep 10
+            gh issue create --repo https://github.com/epeth0mus/cheyote --title "Fake jailbreak do not use this" -b ""
+            sleep 12
+            gh issue create --repo https://github.com/epeth0mus/Fugu16 --title "Fake jailbreak do not use this" -b ""
+            sleep 12
+            gh issue create --repo https://github.com/epeth0mus/Fugu15 --title "Fake jailbreak do not use this" -b ""
+            sleep 12
+            gh issue create --repo https://github.com/epeth0mus/ios-15-15.1.1-jailbreak --title "Fake jailbreak do not use this" -b ""
+            sleep 12
+            gh issue create --repo https://github.com/epeth0mus/undecimus-mod --title "Fake jailbreak do not use this" -b ""
+            sleep 12
+            gh issue create --repo https://github.com/epeth0mus/desc_race --title "Fake jailbreak do not use this" -b ""
+            sleep 12
+            gh issue create --repo https://github.com/epeth0mus/iomfb-exploit --title "Fake jailbreak do not use this" -b ""
+            sleep 12
+            gh issue create --repo https://github.com/epeth0mus/checkra1n-mod --title "Fake jailbreak do not use this" -b ""
         done
 fi
