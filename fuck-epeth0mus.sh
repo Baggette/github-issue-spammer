86cb093f8a61562caf6eb52264a4629b658a8e00
Baggette <play.email200620062006@gmail.com>
2022-08-27 10:53:15 -0400

    (maybe) fixes my idiocy

--- fuck-epeth0mus.sh
+++ fuck-epeth0mus.sh
@@ -4,16 +4,16 @@ sudo apt update && sudo apt upgrade -y && sudo apt install gh
 echo "Have you configured gh yet? y/n"
 read y
 if [[$y == "n"]]; then
-        echo "It is reccomened to have a personal access token ready"
-        sleep 2
-        gh auth login
-        sleep 2
-        echo "Please rerun the script"
-        sleep 2
-        fi
+    echo "It is reccomened to have a personal access token ready"
+    sleep 2
+    gh auth login
+    sleep 2
+    echo "Please rerun the script"
+    sleep 2
+    fi
 elif [[$y == "y"]]; then
-        while true
-        do
-            gh issue create --repo https://github.com/Baggette/fuck-epeth0mus --title "test"
-            sleep 10
-        done
+    while true
+    do
+        gh issue create --repo https://github.com/Baggette/fuck-epeth0mus --title "test"
+        sleep 10
+    done
